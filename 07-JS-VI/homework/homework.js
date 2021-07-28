c7ad0debe3147f8ab3eb4dc2453471cd3995d218
Agustin Sartorio <sartorio85@gmail.com>
2021-07-28 19:05:59 -0300

    finished homework 07

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -44,7 +44,10 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  var nuevoArray = array.map(function (cb) {return cb (cb);}); return nuevoArray;
+  var nuevoArray = array.map(function (elemento) {
+    return cb (elemento);
+  })
+  return nuevoArray
 }
 
 function filter(array) {
